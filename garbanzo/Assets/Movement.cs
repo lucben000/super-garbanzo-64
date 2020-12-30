6dd21dac803dd3ccd7035e5b1cf0fccf6f253f07
lucben000 <lucben000@gmail.com>
2020-12-29 20:16:42 -0800

    Added smooth movement to the square

--- garbanzo/Assets/Movement.cs
+++ garbanzo/Assets/Movement.cs
@@ -1,35 +1,35 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
-    public Vector2 Position;
+    public Vector2 position;
+    public Vector2 movement;
+    public float movementSpeed = 5f;
+    public Rigidbody2D rb;
 
     // Start is called before the first frame update
     void Start()
     {
-        Position = new Vector2(4.28f, 1.28f);
+        position = new Vector2(4.28f, 1.28f);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown("up"))
-        {
-            transform.position = new Vector2(transform.position.x, transform.position.y + 0.5f);
-        }
-        if (Input.GetKeyDown("down"))
-        {
-            transform.position = new Vector2(transform.position.x, transform.position.y - 0.5f);
-        }
-        if (Input.GetKeyDown("left"))
-        {
-            transform.position = new Vector2(transform.position.x - 0.5f, transform.position.y);
-        }
-        if (Input.GetKeyDown("right"))
-        {
-            transform.position = new Vector2(transform.position.x + 0.5f, transform.position.y);
-        }
+        //Up and Down inputs (W and S)
+        movement.y = Input.GetAxisRaw("Vertical");
+
+        //Left and Right inputs (A and D)
+        movement.x = Input.GetAxisRaw("Horizontal");
+    }
+
+    //Fixed Update called 50 times a second
+    private void FixedUpdate()
+    {
+        //Movement for the player
+        rb.MovePosition(rb.position + movement * movementSpeed * Time.fixedDeltaTime);
     }
 }
